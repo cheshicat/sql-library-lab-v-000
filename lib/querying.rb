d47831f1c63e918047654bb074385299806f4698
Allison Howard <alliemhow@gmail.com>
2018-12-29 05:28:40 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,8 +29,13 @@ end
 
 def select_name_and_series_subgenres_of_authors
   <<-SQL
+    SELECT authors.name, subgenres.name
+    FROM authors
+    JOIN series
+    ON series.author_id = authors.id
+    JOIN subgenres
+    ON series.subgenre_id = subgenres.id
   SQL
-  "SELECT authors.name, subgenres.name FROM authors JOIN series ON series.author_id = authors.id JOIN subgenres ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
